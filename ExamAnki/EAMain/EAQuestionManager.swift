024985a0ce4bad04c6cd3d7456e4cdea89a57d91
Jaykong <kongyunpeng2011@sina.com>
2016-03-11 04:02:11 +0800

    correct Papers

---
+++ ExamAnki/EAMain/EAQuestionManager.swift
@@ -0,0 +1,40 @@
+//
+//  EAQuestionManager.swift
+//  ExamAnki
+//
+//  Created by kongyunpeng on 3/11/16.
+//  Copyright © 2016 kongyunpeng. All rights reserved.
+//
+
+import UIKit
+
+class EAQuestionManager: NSObject {
+    class func getOneQuestion(qid:String) -> Question? {
+        let request = NSFetchRequest(entityName: EAQuestion)
+        let predicate = NSPredicate(format: "id like %@", qid)
+        request.predicate = predicate
+        do {
+            let questions = try CoreDataStack.sharedCoreDataStack.mainQueueContext.executeFetchRequest(request)
+            if !questions.isEmpty {
+                return questions[0] as? Question
+            }
+            
+        } catch {
+            print(error)
+        }
+        return nil
+    }
+    class  func getQuestionsWithTypeCode(typeCode:Int) -> [Question]? {
+        let request = NSFetchRequest(entityName: EAQuestion)
+        let predicate = NSPredicate(format: "typecode==%i", typeCode)
+        request.predicate = predicate
+        do {
+            let questions = try CoreDataStack.sharedCoreDataStack.mainQueueContext.executeFetchRequest(request)
+            return questions as? [Question]
+            
+        } catch {
+            print(error)
+        }
+        return nil
+    }
+}
