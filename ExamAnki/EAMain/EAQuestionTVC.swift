a0acd00cb6feeaf229eeb92b95e96367c810cae6
Jaykong <kongyunpeng2011@sina.com>
2016-03-14 10:45:24 +0800

    EAQuestionTVC Test

---
+++ ExamAnki/EAMain/EAQuestionTVC.swift
@@ -0,0 +1,110 @@
+//
+//  EAQuestionTVC.swift
+//  ExamAnki
+//
+//  Created by trainer on 3/14/16.
+//  Copyright © 2016 kongyunpeng. All rights reserved.
+//
+
+import UIKit
+
+class EAQuestionTVC: UITableViewController {
+    var question = EAQuestionManager.getOneQuestion("2015012115523001882030d-bff093e5b7f8")
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 4
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if (section == 1) {
+            return (question?.options?.count)!
+        }
+        
+        return 1
+    }
+
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+        switch indexPath.section {
+        case 0:
+            cell.textLabel?.text = question?.title
+            
+        case 1:
+            let sets = question?.options!
+            let options = Array (sets!)
+            question?.options
+            cell.textLabel?.text = options[indexPath.row] as? String
+        case 2:
+            cell.textLabel?.text = question?.answer
+        case 3:
+            cell.textLabel?.text = question?.parse
+        default:
+            break
+        }
+        
+        // Configure the cell...
+
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
