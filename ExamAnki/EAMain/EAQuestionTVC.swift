268b160343fd2afc8cb729d478e740ddd2d09fcd
Xin quan <sim.quantum@gmail.com>
2016-03-21 15:19:34 +0800

    EAQuestion TCV

--- ExamAnki/EAMain/EAQuestionTVC.swift
+++ ExamAnki/EAMain/EAQuestionTVC.swift
@@ -27,12 +27,29 @@ class EAQuestionTVC: UITableViewController{
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    // MARK: - register nib
+    func registerForNib() {
+        let nib = UINib(nibName: "EAQuestionTVCell", bundle: NSBundle.mainBundle())
+        let nib1 = UINib(nibName: "EAAnswerTVCell", bundle: NSBundle.mainBundle())
+        let nib2 = UINib(nibName: "EAReaultTVCell", bundle: NSBundle.mainBundle())
+        let nib3 = UINib(nibName: "EAReferenceTVCell", bundle: NSBundle.mainBundle())
+        let nib4 = UINib(nibName: "EAAnalysisTVCell", bundle: NSBundle.mainBundle())
+        let nib5 = UINib(nibName: "EADefultTVCell", bundle: NSBundle.mainBundle())
+        tableView.registerNib(nib, forCellReuseIdentifier: "EAQuestionTVCell")
+        tableView.registerNib(nib1, forCellReuseIdentifier: "EAAnswerTVCell")
+        tableView.registerNib(nib2, forCellReuseIdentifier: "EAReaultTVCell")
+        tableView.registerNib(nib3, forCellReuseIdentifier: "EAReferenceTVCell")
+        tableView.registerNib(nib4, forCellReuseIdentifier: "EAAnalysisTVCell")
+        tableView.registerNib(nib5, forCellReuseIdentifier: "EADefultTVCell")
+    }
     // MARK: - Table view data source
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 4
+        if question!.parse!.isEmpty {
+            return 4
+        }
+        return 5
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -45,37 +62,66 @@ class EAQuestionTVC: UITableViewController{
 
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+        
         switch indexPath.section {
             
         case 0:
-            cell.textLabel?.text = "\(question.sort!)、\(question.title!)"
-            cell.textLabel?.numberOfLines = 0
-            
+            let cell = tableView.dequeueReusableCellWithIdentifier("EAQuestionTVCell", forIndexPath: indexPath) as! EAQuestionTVCell
+            cell.questionLbl?.text = "\(question.sort!)、\(question.title!)"
+            cell.questionLbl?.numberOfLines = 0
+            return cell
         case 1:
+            let cell = tableView.dequeueReusableCellWithIdentifier("EAAnswerTVCell", forIndexPath: indexPath) as! EAAnswerTVCell
             let sets = question?.options!
-            let options = Array (sets!)
-            question?.options
-            let option = options[indexPath.row] as! QuestionOption
+            let options = Array (sets!) as NSArray
+            //  options.sortedArrayUsingSelector("caseInsensitiveCompare:")
+            let sortDes = NSSortDescriptor(key: "sort", ascending: true)
+            let newOptions = options.sortedArrayUsingDescriptors([sortDes])
+            
+            // question?.options
+            let option = newOptions[indexPath.row] as! QuestionOption
             let title = option.content
-            cell.textLabel?.text = title
+            cell.answerLbl?.text = title
+            return cell
         case 2:
-            cell.textLabel?.text = question?.answer
+            let cell = tableView.dequeueReusableCellWithIdentifier("EAAnalysisTVCell", forIndexPath: indexPath) as! EAAnalysisTVCell
+            cell.anslysisLbl?.text = "答对了"
+            cell.anslysisLbl?.numberOfLines = 0
+            return cell
         case 3:
-            cell.textLabel?.text = question?.parse
-            cell.textLabel?.numberOfLines = 0
-
+            let cell = tableView.dequeueReusableCellWithIdentifier("EAReaultTVCell", forIndexPath: indexPath) as! EAReaultTVCell
+            cell.reaultLbl?.text = "参考答案 \(question.answer!)"
+            return cell
+        case 4:
+            let cell = tableView.dequeueReusableCellWithIdentifier("EAReferenceTVCell", forIndexPath: indexPath) as! EAReferenceTVCell
+            if question?.parse == nil {
+                cell.referenceLbl?.text = " "
+            } else {
+                cell.referenceLbl?.text = question?.parse
+                
+            }
+            
+            cell.referenceLbl?.numberOfLines = 0
+            return cell
+            
         default:
-            break
+            //            let cell = tableView.dequeueReusableCellWithIdentifier("EADefultTVCell") as! EADefultTVCell
+            //            cell.defultLbl!.text = "No answer!"
+            //            return cell
+            
+            let cell = tableView.dequeueReusableCellWithIdentifier("EAReferenceTVCell", forIndexPath: indexPath) as! EAReferenceTVCell
+            cell.referenceLbl?.text = question?.parse
+            cell.referenceLbl?.numberOfLines = 0
+            return cell
         }
-        
-        // Configure the cell...
-
-        return cell
     }
     
     override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        return UITableViewAutomaticDimension
+        if ((question?.parse) == nil) {
+            return 44
+        } else {
+            return UITableViewAutomaticDimension
+        }
     }
     
 
